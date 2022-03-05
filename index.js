77d8935d9bb23138d243f4b8800717c7361440cc
Kostiantyn Stoliarskyi <saintkos117@gmail.com>
2022-03-05 12:29:23 +0200

    start_with_vanile

---
+++ index.js
@@ -0,0 +1,7 @@
+const mgcBtn = document.querySelector(".btn");
+const liItem = document.querySelector(".liprt");
+const imgEl = document.querySelector(".imghere");
+
+mgcBtn.addEventListener("click", () => {
+  imgEl.src = "./10-1.png";
+});
