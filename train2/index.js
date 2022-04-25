6e88b013319040fd8c9fe927070bfd68c4ee9492
Костя Столярский <saintkos117@gmail.com>
2022-04-25 08:53:21 +0300

    Update index.js

--- train2/index.js
+++ train2/index.js
@@ -1,3 +1,23 @@
+const promise2 = new Promise((resolve, reject) => {
+  const cafill = Math.random() > 0.5;
+  setTimeout(() => {
+    if (cafill) {
+      resolve("first");
+    } else {
+      reject("second");
+    }
+  }, 300);
+});
+
+promise2.then(onFullFilled2, onRejected2);
+
+function onFullFilled2(result) {
+  console.log("This onFullFilled2:", result);
+}
+function onRejected2(result) {
+  console.log("This onRejected2:", result);
+}
+
 const promise = new Promise((resolve, reject) => {
   const cafill = Math.random() > 0.5;
   setTimeout(() => {
@@ -17,3 +37,7 @@ function onFullFilled(result) {
 function onRejected(result) {
   console.log("This onRejected:", result);
 }
+
+console.log(5 === "5");
+console.log("this var ole:", ole);
+var ole = 5;
